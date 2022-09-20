a84185256642127eddc870a7b412dcf393aade00
go214621 <113402561+go214621@users.noreply.github.com>
2022-09-20 12:49:02 -0500

    add two new functions for percent error and density

--- include/headers/myMath.h
+++ include/headers/myMath.h
@@ -16,6 +16,35 @@ float getHypotenuse() {
     std::cout << "The hypotenuse length is: " << std::hypot(a, b) << std::endl; 
     return std::hypot(a, b);   
 }
+
+
+float getPercentError() {
+    float a = 0;
+    float b = 0;
     
+    std::cout << "Enter the main number:" << std::endl;
+    std::cin >> a;
+    std::cout << "Enter the differing number:" << std::endl;
+    std::cin >> b;
+    
+    float ans = ((fabs(a - b) / a) * 100);
+    std::cout << "The percent error is: " << ans << std::endl;
+    return ans;
+}
+
+float getDensity() {
+
+    float mass = 0;
+    float volume = 0;
+    
+    std::cout << "Enter the mass:" << std::endl;
+    std::cin >> mass;
+    std::cout << "Enter the volume:" << std::endl;
+    std::cin >> volume;
+    
+    float density = mass / volume;
+    std::cout << "The density is: " << density << std::endl;
+    return density;
+}
 
 #endif
