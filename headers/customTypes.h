24f861852f9e32adc11ab938fcb8eefeaef1d275
go214621 <113402561+go214621@users.noreply.github.com>
2022-10-20 10:00:43 -0500

    Update customTypes.h

--- headers/customTypes.h
+++ headers/customTypes.h
@@ -1,10 +1,28 @@
 #pragma once
 #ifndef CUSTOM_TYPE_H
 #define CUSTOM_TYPE_H
-
+#include <cmath>
+#include "periodicTable.h"
+// graphing
 struct Vec2f {
     float x = 0.0;
     float y = 0.0;
 };
+
+enum MirrorAxis {
+    MAxis_X,         // reflect over X axis; (a, -b);
+    MAxis_Y,         // reflect over Y axis; (-a, b);
+    MAxis_YEqX,      // y = x;               (b, a);
+    MAxis_YEqNegX,   // y = -x;              (-b, -a);
+    MAxis_Origin,    // x and y are negated; (-a, -b);
+};
+
+enum RotationValue {
+    Rot_90,
+    Rot_180,
+    Rot_270,
+};
+// chemistry
+const float avaNum = 6.02 * pow(10, 23);
 
 #endif
