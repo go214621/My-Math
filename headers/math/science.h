86e8dc49e10614423b630fc2a0c5fee14d36ce73
go214621 <113402561+go214621@users.noreply.github.com>
2023-01-09 10:29:15 -0600

    Create science.h

---
+++ headers/math/science.h
@@ -0,0 +1,55 @@
+#pragma once
+#ifndef SCIENCE_MATH_H
+#define SCIENCE_MATH_H
+
+#include <cmath>
+#include "../customTypes.h"
+#include <iostream>
+
+float getPercentError() {
+    float a = 0, b = 0;
+    
+    std::cout << "Enter the main number:" << std::endl;
+    std::cin >> a;
+    std::cout << "Enter the differing number:" << std::endl;
+    std::cin >> b;
+    
+    float ans = ((fabs(a - b) / a) * 100);
+    std::cout << "The percent error is: " << ans << std::endl;
+    return ans;
+}
+
+float getDensity() {
+
+    float mass = 0, volume = 0;
+    
+    std::cout << "Enter the mass:" << std::endl;
+    std::cin >> mass;
+    std::cout << "Enter the volume:" << std::endl;
+    std::cin >> volume;
+    
+    float density = mass / volume;
+    std::cout << "The density is: " << density << std::endl;
+    return density;
+}
+
+float getPercentAbundance() {
+    
+    float individualAtomCount = 0, totalAtomCount = 0;
+    
+    std::cout << "Enter the number of atoms of one particular isotope: " << std::endl;
+    std::cin >> individualAtomCount;
+    std::cout << "Enter the total number of atoms: " << std::endl;
+    std::cin >> totalAtomCount;
+    
+    float percentAbundance = (individualAtomCount / totalAtomCount) * 100;
+    std::cout << "The percent abundance is: " << percentAbundance << std::endl;
+    return percentAbundance;
+}
+
+float frequencyToEnergy(float frequency, bool print = false) {
+    if (print)
+        std::cout << frequency * planckConst << std::endl;
+    return frequency * planckConst;
+}
+#endif
